f4cc7143bb6da86476b3f4512d0e87b2b1d529d5
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2021-02-16 16:30:36 +0900

    Update 2884.py
    https://www.acmicpc.net/problem/2884

--- 2884.py
+++ 2884.py
@@ -1,10 +1,9 @@
-a, b = input().split( )
-a = int(a)
-b = int(b)
-b = b-45
-if b < 0:
-    b = b + 60
-    a = a - 1
-    if a < 0:
-        a = a + 24
-print(a,b)
+import sys
+
+N = sys.stdin.readline().rstrip()
+N = int(N)
+for i in range(1,N+1):
+    A, B = sys.stdin.readline().rstrip().split( )
+    A = int(A)
+    B = int(B)
+    print(A+B)
