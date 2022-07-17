e87d18f8dc5723cf88ef9907c8ec0dda0a3afe77
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2022-07-17 18:23:35 +0900

    구현 및 풀이 과정 추가

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -7,12 +7,13 @@ cam_angle = {1: ['U', 'D', 'L', 'R'], 2: ['UD', 'LR'], 3: ['UR', 'RD', 'DL', 'LU
 N, M = map(int, sys.stdin.readline().split())
 global office
 office = [list(map(int, sys.stdin.readline().split())) for i in range(N)]
-cam_info = [(office[i][j], (i, j)) for i in range(N) for j in range(M) if 1 <= office[i][j] <= 5]
-cam_count = len(cam_info)
+cam_info = [(office[i][j], (i, j)) for i in range(N) for j in range(M) if
+            1 <= office[i][j] <= 5]  # 사무실에 존재하는 (카메라 모델 번호, 좌표)를 저장한다.
+cam_count = len(cam_info)  # 사무실에 존재하는 카메라 개수 저장
 ans = []
 
 
-def dfs(cnt):
+def dfs(cnt):  # 카메라의 모델을 depth/ 각각의 방향을 width로하는 상태트리 탐색
     global office
     if cnt == cam_count:
         ans.append(sum([office[i].count(0) for i in range(N)]))
@@ -20,11 +21,11 @@ def dfs(cnt):
     model_num, (x, y) = cam_info[cnt]
     angles = cam_angle[model_num]
     for angle in angles:
-        org = copy.deepcopy(office)
+        org = copy.deepcopy(office)  # 다음 depth의 dfs가 종료되면 원상태로 되돌아 오기 위해 원보 사무실을 저장한다.
         for di in angle:
             dx, dy = di2v[di]
             tmp_x, tmp_y = x, y
-            while 1:
+            while 1: #카메라가 볼 수 있는 영역 체크
                 nx = tmp_x + dx
                 ny = tmp_y + dy
                 if 0 <= nx < N and 0 <= ny < M:
@@ -37,6 +38,7 @@ def dfs(cnt):
                 tmp_x, tmp_y = nx, ny
         dfs(cnt + 1)
         office = copy.deepcopy(org)
+
 
 dfs(0)
 print(min(ans))
