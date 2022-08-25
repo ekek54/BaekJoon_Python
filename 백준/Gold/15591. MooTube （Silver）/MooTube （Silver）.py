c3c784cb3deac477f8c49a62ec0cf78308ad1669
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2022-08-25 20:59:37 +0900

    [Gold V] Title: MooTube (Silver), Time: 5528 ms, Memory: 371720 KB -BaekjoonHub

---
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
@@ -0,0 +1,49 @@
+import sys
+sys.setrecursionlimit(5000)
+
+def dfsVideoTree(curVideo, visitVideo, recVideoNum, videoTree_adjDict, K):
+    visitVideo[curVideo] = True
+
+    if isLastDepthVideo(curVideo, visitVideo, videoTree_adjDict):
+        return recVideoNum
+
+    for adjInfo in videoTree_adjDict[curVideo]:
+        adjVideo, usado = adjInfo
+        if visitVideo[adjVideo]:
+            continue
+        elif usado >= K:
+            recVideoNum += 1
+            recVideoNum = dfsVideoTree(adjVideo, visitVideo, recVideoNum, videoTree_adjDict, K)
+    return recVideoNum
+
+
+def isLastDepthVideo(video, visitVideo, VideoTree_adjDict):
+    for adjInfo in VideoTree_adjDict[video]:
+        adjVideo, usado = adjInfo
+        if not visitVideo[adjVideo]:
+            return False
+    return True
+
+
+def main():
+    N, Q = map(int, sys.stdin.readline().split())
+    adjDict = {}
+
+    for i in range(N - 1):
+        p, q, r = map(int, sys.stdin.readline().split())
+        if p in adjDict:
+            adjDict[p].append((q, r))
+        if q in adjDict:
+            adjDict[q].append((p, r))
+        if p not in adjDict:
+            adjDict[p] = [(q, r)]
+        if q not in adjDict:
+            adjDict[q] = [(p, r)]
+
+    for i in range(Q):
+        visit = [False for _ in range(N + 1)]
+        k, v = map(int, sys.stdin.readline().split())
+        print(dfsVideoTree(v, visit, 0, adjDict, k))
+
+
+main()
