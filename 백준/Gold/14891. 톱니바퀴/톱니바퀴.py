376938fc16775fdfea02bce875c48da73c637731
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2023-01-02 11:04:03 +0900

    [Gold V] Title: 톱니바퀴, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,45 +1,57 @@
 import sys
 
-wheel_list = [{'state': list(map(int, list(sys.stdin.readline().rstrip('\n')))), 'left': 6, 'right': 2, 'top': 0} for _
-              in
-              range(4)]
+wheel_list = [
+  {'state': list(map(int, list(sys.stdin.readline().rstrip('\n')))), 'left': 6, 'right': 2, 'top': 0, 'visit': False}
+  for _
+  in
+  range(4)]
 wheel_size = 8
 N = int(sys.stdin.readline())
-visit = [False for _ in range(4)]
 score = 0
 
 
 # 톱니바퀴 회전
 def rotate(wheel_num, clockwise):
+  wheel = wheel_list[wheel_num]
   if clockwise == 1:
-    wheel_list[wheel_num]['left'] = (wheel_list[wheel_num]['left'] - 1) % wheel_size
-    wheel_list[wheel_num]['right'] = (wheel_list[wheel_num]['right'] - 1) % wheel_size
-    wheel_list[wheel_num]['top'] = (wheel_list[wheel_num]['top'] - 1) % wheel_size
+    wheel['left'] = (wheel['left'] - 1) % wheel_size
+    wheel['right'] = (wheel['right'] - 1) % wheel_size
+    wheel['top'] = (wheel['top'] - 1) % wheel_size
   else:
-    wheel_list[wheel_num]['left'] = (wheel_list[wheel_num]['left'] + 1) % wheel_size
-    wheel_list[wheel_num]['right'] = (wheel_list[wheel_num]['right'] + 1) % wheel_size
-    wheel_list[wheel_num]['top'] = (wheel_list[wheel_num]['top'] + 1) % wheel_size
+    wheel['left'] = (wheel['left'] + 1) % wheel_size
+    wheel['right'] = (wheel['right'] + 1) % wheel_size
+    wheel['top'] = (wheel['top'] + 1) % wheel_size
+
+
+def init_visit():
+  for i in range(4):
+    wheel_list[i]['visit'] = False
 
 
 # 회전의 전파후 회전
 def propagate(wheel_num, clockwise):
-  visit[wheel_num] = True
+  cur_wheel = wheel_list[wheel_num]
+  cur_wheel['visit'] = True
   if 1 <= wheel_num:
-    if not visit[wheel_num - 1]:
-      if wheel_list[wheel_num]['state'][wheel_list[wheel_num]['left']] != wheel_list[wheel_num - 1]['state'][
-        wheel_list[wheel_num - 1]['right']]:
+    left_wheel = wheel_list[wheel_num - 1]
+    if not left_wheel['visit']:
+      if cur_wheel['state'][cur_wheel['left']] != left_wheel['state'][left_wheel['right']]:
         propagate(wheel_num - 1, clockwise * -1)
   if wheel_num < 3:
-    if not visit[wheel_num + 1]:
-      if wheel_list[wheel_num]['state'][wheel_list[wheel_num]['right']] != wheel_list[wheel_num + 1]['state'][
-        wheel_list[wheel_num + 1]['left']]:
+    right_wheel = wheel_list[wheel_num + 1]
+    if not right_wheel['visit']:
+      if cur_wheel['state'][cur_wheel['right']] != right_wheel['state'][right_wheel['left']]:
         propagate(wheel_num + 1, clockwise * -1)
   rotate(wheel_num, clockwise)
   return
 
 
+def calc_score():
+  return sum([wheel_list[i]['state'][wheel_list[i]['top']] * (2 ** i) for i in range(4)])
+
+
 for i in range(N):
-  visit = [False for _ in range(4)]
+  init_visit()
   wheel_num, clockwise = map(int, sys.stdin.readline().split())  # 바퀴 번호 1 ~ 4, 시계방향 1, 반시계 -1
   wheel_num -= 1  # 0 ~ 3
   propagate(wheel_num, clockwise)
@@ -50,4 +62,4 @@ for i in range(4):
   if wheel_list[i]['state'][top] == 1:
     score += 2 ** i
 
-print(score)
+print(calc_score())
