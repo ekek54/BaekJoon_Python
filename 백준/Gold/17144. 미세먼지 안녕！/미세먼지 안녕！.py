44eed65d7200a798d9498057999008e1bcbf20f4
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2023-01-27 10:41:23 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 360 ms, Memory: 116412 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,55 +1,79 @@
 import sys
-import copy
 
-dr = [0, 0, 1, -1]
-dc = [1, -1, 0, 0]
-R, C, T = map(int,sys.stdin.readline().split())
-room = [list(map(int,sys.stdin.readline().split())) for _ in range(R)]
+R, C, T = map(int, sys.stdin.readline().split())
+A = [list(map(int, sys.stdin.readline().split())) for _ in range(R)]
+dr = [1, -1, 0, 0]
+dc = [0, 0, 1, -1]
+breaker = False
+for i in range(R):
+  for j in range(C):
+    if A[i][j] == -1:
+      cleaner_top_rc = tuple([i, j])
+      cleaner_bot_rc = tuple([i + 1, j])
+      breaker = True
+      break
+  if breaker:
+    break
 
-def dust(board):
-    org=copy.deepcopy(board)
-    for i in range(R):
-        for j in range(C):
-            if board[i][j]>=5:
-                move_dust=org[i][j]//5
-                for k in range(4):
-                    nr = i + dr[k]
-                    nc = j + dc[k]
-                    if 0<=nr<R and 0<=nc<C and board[nr][nc] != -1:
-                        board[nr][nc] += move_dust
-                        board[i][j] -= move_dust
-    return
+#print(cleaner_bot_rc, cleaner_top_rc)
 
-def spin(board,r,c,clock):
-    di = [(0, 1), (-1, 0), (0, -1), (1, 0)] if clock else [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    tmp=0
-    idx=0
-    while 1:
-        nr = r + di[idx][0]
-        nc = c + di[idx][1]
-        if 0 <= nr < R and 0 <= nc < C:
-            if board[nr][nc] == -1:
-                break
-            board[nr][nc], tmp = tmp, board[nr][nc]
-            r, c = nr, nc
-        else:
-            idx+=1
-    return
+def spread(A):
+  tmp_board = [[0 for j in range(C)] for i in range(R)]
+  for i in range(R):
+    for j in range(C):
+      if A[i][j] and A[i][j] != -1:
+        # 확산된 칸 수
+        cnt = 0
+        # 확산 될 미세먼지 값
+        spread_dust = A[i][j] // 5
+        for k in range(4):
+          nr = i + dr[k]
+          nc = j + dc[k]
+          if 0 <= nr < R and 0 <= nc < C and A[nr][nc] != -1:
+            cnt += 1
+            tmp_board[nr][nc] += spread_dust
+        A[i][j] -= (spread_dust * cnt)
+  for i in range(R):
+    for j in range(C):
+      A[i][j] += tmp_board[i][j]
+  return A
 
 
-def air(board):
-    for i in range(R):
-        if board[i][0] == -1:
-            ac_top = (i,0)
-            ac_bottom = (i+1,0)
-            break
-    spin(board, *ac_top, True)
-    spin(board, *ac_bottom, False)
-    return
+def cleaner_rotate(A, rc, clockwise):
+  clock = [2, 0, 3, 1]
+  counterclock = [2, 1, 3, 0]
+  if clockwise:
+    rotate_di = clock
+  else:
+    rotate_di = counterclock
+  di = 0
+  cur_r, cur_c = rc
+  tmp = 0
+  while True:
+    nr = cur_r + dr[rotate_di[di]]
+    nc = cur_c + dc[rotate_di[di]]
+    # 이동
+    if 0 <= nr < R and 0 <= nc < C:
+      if A[nr][nc] == -1:
+        break
+      A[nr][nc], tmp = tmp, A[nr][nc]
+      cur_r, cur_c = nr, nc
+    # 방향전환
+    else:
+      di += 1
+  return A
 
-while T>0:
-    dust(room)
-    air(room)
-    T-=1
+time = 0
+while time < T:
+  spread(A)
+  cleaner_rotate(A, cleaner_top_rc, False)
+  cleaner_rotate(A, cleaner_bot_rc, True)
+  time += 1
 
-print(sum(map(sum,room))+2)
+ans = 0
+
+for i in range(R):
+  for j in range(C):
+    if A[i][j] != -1:
+      ans += A[i][j]
+print(ans)
