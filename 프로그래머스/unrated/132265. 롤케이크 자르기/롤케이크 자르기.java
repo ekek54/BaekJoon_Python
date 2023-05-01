1153425a0e79c178d99ac9fc1484303fa7ba7296
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2023-05-01 18:50:19 +0900

    [unrated] Title: 롤케이크 자르기, Time: 239.58 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,38 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(int[] topping) {
-        int answer = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < topping.length; i ++) {
-            if (map.containsKey(topping[i])) {
-                map.put(topping[i], map.get(topping[i]) + 1);
-            }
-            else{
-                map.put(topping[i], 1);
-            }
-        }
-        
-        HashMap<Integer, Integer> remain = new HashMap<>();
-        for(int i = 0; i < topping.length; i ++) {
-            // A에서 제외
-            if (map.get(topping[i]) == 1){
-                map.remove(topping[i]);
-            }
-            else{
-                map.put(topping[i], map.get(topping[i]) - 1);
-            }
-            // B에 추가
-            if (remain.containsKey(topping[i])){
-                remain.put(topping[i], remain.get(topping[i]) + 1);
-            }
-            else{
-                remain.put(topping[i], 1);
-            }
-            
-            if (map.size() == remain.size()){
-                answer += 1;
-            }
-        }
+        int answer = -1;
         return answer;
     }
 }
