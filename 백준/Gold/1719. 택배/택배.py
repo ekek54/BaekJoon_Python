d8f4887c89d7e9bb6d911bceef646d3995dc9992
김강년 <64246800+ekek54@users.noreply.github.com>
2024-02-29 16:02:20 +0900

    [Gold III] Title: 택배, Time: 312 ms, Memory: 113224 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.py
+++ 백준/Gold/1719. 택배/택배.py
@@ -1,47 +1,34 @@
 import sys
 from math import inf
-
 n, m = map(int, sys.stdin.readline().split())
 adj_matrix = [[inf for j in range(n)] for i in range(n)]
-for i in range(n):
-  adj_matrix[i][i] = 0
-
-# i -> j 경로의 경유지
-stopover = [[-1 for j in range(n)] for i in range(n)]
-for i in range(n):
-  stopover[i][i] = i
-
-
-for _ in range(m):
-  a, b, t = map(int, sys.stdin.readline().split())
-  a -= 1
-  b -= 1
-  adj_matrix[a][b] = t
-  adj_matrix[b][a] = t
-  stopover[a][b] = b
-  stopover[b][a] = a
-
-# 플로이드-워셜
-for k in range(n):
-  for i in range(n):
-    for j in range(n):
-      if adj_matrix[i][j] > adj_matrix[i][k] + adj_matrix[k][j]:
-        adj_matrix[i][j] = adj_matrix[i][k] + adj_matrix[k][j]
-        stopover[i][j] = k
-
-# i에서 j로의 경로에서 i 다음 경유지
-def nxt_node(i, j):
-  if stopover[i][j] == j:
-    return j
-  else:
-    return nxt_node(i, stopover[i][j])
-
 answer = [['-' for j in range(n)] for i in range(n)]
+for _ in range(m):
+    a, b, d = map(int, sys.stdin.readline().split())
+    a -= 1
+    b -= 1
+    answer[a][b] = b
+    answer[b][a] = a
+    adj_matrix[a][b] = d
+    adj_matrix[b][a] = d
 
 for i in range(n):
-  for j in range(n):
-    if i == j: continue
-    answer[i][j] = nxt_node(i, j) + 1
+    adj_matrix[i][i] = 0
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if adj_matrix[i][j] > adj_matrix[i][k] + adj_matrix[k][j]:
+                adj_matrix[i][j] = adj_matrix[i][k] + adj_matrix[k][j]
+                answer[i][j] = answer[i][k]
 
 for i in range(n):
-  print(*answer[i])
+    for j in range(n):
+        if answer[i][j] == '-': continue
+        answer[i][j] += 1
+
+def pb(board):
+    for i in range(len(board)):
+        print(*board[i])
+
+pb(answer)
