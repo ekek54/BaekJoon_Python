2b59a45055156bfe0273d657bdd0e9a516701b4c
김강년 <ekek54@ajou.ac.kr>
2025-04-10 20:52:42 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 147.90 ms, Memory: 64.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = st.nextToken();
+        int n = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            System.out.print(str);
+        }
+    }
+}
