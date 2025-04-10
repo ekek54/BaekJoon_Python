89e880679b964443df174c5e75ec0d5e9630531f
김강년 <ekek54@ajou.ac.kr>
2025-04-10 20:45:02 +0900

    [level 0] Title: 문자열 출력하기, Time: 160.80 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(input);
+    }
+}
